a164e0a69d742fd1e934ab52b06a27b6519c6f51
Cosmin Parvulescu <Cosmin.Parvulescu@teamnet.ro>
2014-06-25 19:06:24 +0300

    Added timetables to a separate script file.

---
+++ timetables.js
@@ -0,0 +1,35 @@
+var unirii_greengate = [
+      moment({ hour: 08, minute: 00 }),
+      moment({ hour: 08, minute: 15 }),
+      moment({ hour: 08, minute: 30 }),
+      moment({ hour: 08, minute: 40 }),
+      moment({ hour: 08, minute: 50 }),
+      moment({ hour: 09, minute: 00 }),
+      moment({ hour: 09, minute: 10 }),
+      moment({ hour: 09, minute: 20 }),
+      moment({ hour: 09, minute: 30 }),
+      moment({ hour: 09, minute: 45 }),
+      moment({ hour: 09, minute: 50 }),
+      moment({ hour: 10, minute: 00 }),
+      moment({ hour: 10, minute: 10 }),
+      moment({ hour: 10, minute: 20 }),
+      moment({ hour: 10, minute: 40 })
+];
+
+var greengate_unirii = [
+      moment({ hour: 17, minute: 15 }),
+      moment({ hour: 17, minute: 30 }),
+      moment({ hour: 18, minute: 00 }),
+      moment({ hour: 18, minute: 10 }),
+      moment({ hour: 18, minute: 20 }),
+      moment({ hour: 18, minute: 30 }),
+      moment({ hour: 18, minute: 45 }),
+      moment({ hour: 18, minute: 50 }),
+      moment({ hour: 19, minute: 00 }),
+      moment({ hour: 19, minute: 10 }),
+      moment({ hour: 19, minute: 20 }),
+      moment({ hour: 19, minute: 30 }),
+      moment({ hour: 19, minute: 40 }),
+      moment({ hour: 19, minute: 50 }),
+      moment({ hour: 20, minute: 05 })
+];
